e2101c4cd3c36c69b2c0ac6b6fd479f50262d191
Quang Tran <qmtran@memphis.edu>
2015-04-13 15:20:33 -0500

    add Shannon_entropy.py

---
+++ Shannon_entropy.py
@@ -0,0 +1,36 @@
+import sys
+from itertools import groupby
+
+def fasta_iter(fasta_name):
+    """
+    given a fasta file. yield tuples of header, sequence
+    """
+    fh = open(fasta_name)
+    # ditch the boolean (x[0]) and just keep the header or sequence since
+    # we know they alternate.
+    faiter = (x[1] for x in groupby(fh, lambda line: line[0] == ">"))
+    for header in faiter:
+        # drop the ">"
+        header = header.next()[1:].strip()
+        # join all sequence lines to one.
+        seq = "".join(s.strip() for s in faiter.next())
+        yield header, seq
+
+def sliding_window(seq, n=2):
+    """Returns a sliding window (of width n) over data from the iterable
+       s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   
+       this function courtesy http://stackoverflow.com/questions/6822725/rolling-or-sliding-window-iterator-in-python
+    """
+    it = iter(seq)
+    result = tuple(islice(it, n))
+    if len(result) == n:
+        yield result    
+    for elem in it:
+        result = result[1:] + (elem,)
+        yield result
+
+fasta = sys.argv[1]
+print fasta
+g = fasta_iter(fasta)
+for v in g:
+    print v[0], len(v[1])
