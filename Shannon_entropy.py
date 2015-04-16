a489c32710c27cdbfaa940f0555c58fc0b42ac23
Quang Tran <qmtran@memphis.edu>
2015-04-15 20:10:15 -0500

    update Shannon_entropy.py

--- Shannon_entropy.py
+++ Shannon_entropy.py
@@ -30,9 +30,33 @@ def sliding_window(seq, n=2):
         result = result[1:] + (elem,)
         yield result
 
+def slidingWindow(sequence,winSize,step=1):
+    """Returns a generator that will iterate through
+    the defined chunks of input sequence.  Input sequence
+    must be iterable."""
+    
+    # Verify the inputs
+    try: it = iter(sequence)
+    except TypeError:
+        raise Exception("**ERROR** sequence must be iterable.")
+    if not ((type(winSize) == type(0)) and (type(step) == type(0))):
+        raise Exception("**ERROR** type(winSize) and type(step) must be int.")
+    if step > winSize:
+        raise Exception("**ERROR** step must not be larger than winSize.")
+    if winSize > len(sequence):
+        raise Exception("**ERROR** winSize must not be larger than sequence length.")
+    
+    # Pre-compute number of chunks to emit
+    numOfChunks = ((len(sequence)-winSize)/step)+1
+    
+    # Do the work
+    for i in range(0,numOfChunks*step,step):
+        yield sequence[i:i+winSize]
+
 def Shannon_entropy(seq, k):
     c = Counter()    
-    for kmer in sliding_window(seq, k):        
+    #for kmer in sliding_window(seq, k):
+    for kmer in slidingWindow(seq, k):
         new_count = c[kmer] + 1
         c[kmer] = new_count
     s = 0
